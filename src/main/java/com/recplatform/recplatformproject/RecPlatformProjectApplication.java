9159cdf6b3c6894cf4b92dd7313d76664cd16463
changkiyun <ekzm7845@naver.com>
2025-01-03 23:41:30 +0900

    :sparkles: Feat: 프로젝트 생성

---
+++ src/main/java/com/recplatform/recplatformproject/RecPlatformProjectApplication.java
@@ -0,0 +1,13 @@
+package com.recplatform.recplatformproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecPlatformProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecPlatformProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recplatform/recplatformproject/RecPlatformProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recplatform.recplatformproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecPlatformProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
